0cd8424c2cefaf7ab38cb6ecbc8f75a3f44ae7fe
Md Robin Rana <robin4500bd@gmail.com>
2021-12-17 22:24:23 +0600

    slider created

--- src/Components/Pages/Home/HomeBanner/HomeBanner.js
+++ src/Components/Pages/Home/HomeBanner/HomeBanner.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Slider from "react-slick";
 import '../../../CustomStyle/Style.css';
 import "slick-carousel/slick/slick.css";
@@ -10,29 +10,35 @@ import pizza4 from '../../../../images/pizza-4.png';
 import pizza5 from '../../../../images/pizza-5.png';
 import pizza6 from '../../../../images/pizza-6.png';
 import pizza7 from '../../../../images/pizza-7.png';
+import nextArrow from '../../../../images/next.png';
 
-const NextArrowIcon = () =>
-{
-    return <h2 className='text-black'>Next</h2>;
-};
+
 const HomeBanner = () =>
 {
+    const sliderRef = useRef();
+    const goToNext = () => {
+        sliderRef.current.slickNext();
+    }
+    const goToPrevious = () => {
+        sliderRef.current.slickPrev();
+    }
     const settings = {
         infinite: true,
-        speed: 500,
+        // speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: true,
-        arrows: true,
-        nextArrow: <NextArrowIcon />
+        // autoplay: true,
+        arrows:false
     };
     return (
         <div>
-            <Slider {...settings}>
+            <Slider ref={sliderRef} {...settings}>
+
+                {/* Slide-1 */}
                 <div>
-                    <div className='flex bg-black py-20 justify-around'>
+                    <div className='md:flex bg-black py-20 justify-around'>
                         <div className='relative'>
-                            <img className='pizza1' src={pizza1} alt="Pizza" />
+                            <img className='pizza1 w-10/12 md:w-full' src={pizza1} alt="Pizza" />
                             <img className='pizza2 absolute' src={pizza2} alt="Pizza" />
                             <img className='pizza3 absolute' src={pizza3} alt="Pizza" />
                             <img className='pizza4 absolute' src={pizza4} alt="Pizza" />
@@ -41,12 +47,14 @@ const HomeBanner = () =>
                             <img className='pizza7 absolute' src={pizza7} alt="Pizza" />
                         </div>
                         <div>
-                            <h1 className='text-9xl font-bold uppercase text-white'>Quality</h1>
-                            <h1 className='text-9xl font-bold uppercase text-white pt-6'>F<span className='text-primary'>oo</span>ds</h1>
-                            <p className='text-3xl tracking-widest pt-6 text-primary'>Healthy Food For Healthy <br /> Body</p>
+                            <h1 className='text-6xl text-center md:text-left md:text-9xl font-bold uppercase text-white'>Quality <span className='visible md:hidden'>F<span className='text-primary'>oo</span>ds</span></h1>
+                            <h1 className='text-4xl md:visible hidden text-center md:text-left md:text-9xl font-bold uppercase text-white pt-6'>F<span className='text-primary'>oo</span>ds</h1>
+                            <p className='text-3xl text-center md:text-left tracking-widest pt-6 text-primary'>Healthy Food For Healthy <br className='hidden md:visible'/> Body</p>
                         </div>
                     </div>
                 </div>
+
+                {/* Slide-2 */}
                 <div>
                     <div className='flex bg-black py-20 justify-around'>
                         <div>
@@ -66,6 +74,8 @@ const HomeBanner = () =>
                     </div>
                 </div>
             </Slider>
+            <img className='nextArrow w-10 hidden md:visible' onClick={goToNext} src={nextArrow} alt="NextArrow" />
+            <img className='prevArrow w-10 hidden md:visible' onClick={goToPrevious} src={nextArrow} alt="NextArrow" />
         </div>
     );
 };
